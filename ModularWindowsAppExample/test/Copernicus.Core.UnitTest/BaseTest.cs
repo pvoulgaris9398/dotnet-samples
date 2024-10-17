60e556e1d1dd99d12b496d9c5b74426969bf4cfa
Pete Voulgaris <petevoulgaris@gmail.com>
2024-10-17 17:46:42 -0400

    updated some unit tests

---
+++ ModularWindowsAppExample/test/Copernicus.Core.UnitTest/BaseTest.cs
@@ -0,0 +1,30 @@
+﻿namespace Copernicus.Core.UnitTest
+{
+    public class BaseTest<T> where T : class
+    {
+        protected void OnArrange(Action? action = null)
+        {
+            action?.Invoke();
+        }
+
+        protected V OnArrange<V>(Func<V> action)
+        {
+            return action.Invoke();
+        }
+
+        protected void OnAct(Action action)
+        {
+            action.Invoke();
+        }
+
+        protected V OnAct<V>(Func<V> action)
+        {
+            return action.Invoke();
+        }
+
+        protected void OnAssert(Action? action = null)
+        {
+            action?.Invoke();
+        }
+    }
+}
--- ModularWindowsAppExample/test/Copernicus.Core.UnitTest/UnitTest1.cs
+++ ModularWindowsAppExample/test/Copernicus.Core.UnitTest/UnitTest1.cs
@@ -1,10 +1,10 @@
 namespace Copernicus.Core.UnitTest;
-
-public class UnitTest1
+public class UnitTest1 : BaseTest<UnitTest1>
 {
     [Fact]
     public void Test1()
     {
-
+        var temp = OnArrange(Array.Empty<string>);
+        var actual = OnAct(Array.Empty<int>);
     }
 }
