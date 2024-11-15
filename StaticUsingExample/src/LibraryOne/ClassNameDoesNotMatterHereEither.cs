964a4894cf8625099472abb088191a65ea376df1
Pete Voulgaris <petevoulgaris@gmail.com>
2024-11-14 21:31:45 -0500

    added .editorconfig, devcontainer, some tweaks

--- StaticUsingExample/src/LibraryOne/ClassNameDoesNotMatterHereEither.cs
+++ StaticUsingExample/src/LibraryOne/ClassNameDoesNotMatterHereEither.cs
@@ -4,7 +4,7 @@
     {
         public static string GetValue(int arg1, Action<string> log)
         {
-            Type t = typeof(ClassNameDoesNotMatterHereEither);
+            var t = typeof(ClassNameDoesNotMatterHereEither);
             log(t.ToString());
             log(nameof(GetValue));
             return $"The value is {arg1}";
--- StaticUsingExample/src/StaticUsingExample.Console/ClassNameDoesNotMatterHere.cs
+++ StaticUsingExample/src/StaticUsingExample.Console/ClassNameDoesNotMatterHere.cs
@@ -1,10 +1,10 @@
 ﻿namespace StaticUsingExample.Console
 {
-    internal class ClassNameDoesNotMatterHere
+    internal sealed class ClassNameDoesNotMatterHere
     {
         internal static string GetValue(int arg1, Action<string> log)
         {
-            Type t = typeof(ClassNameDoesNotMatterHere);
+            var t = typeof(ClassNameDoesNotMatterHere);
             log(t.ToString());
             log(nameof(GetValue));
             return $"The value is {arg1}";
