fa5974fdfc72f1d3cda79920e2ab761017fe5c65
Pete Voulgaris <petevoulgaris@gmail.com>
2025-03-19 21:08:03 -0400

    making moduleManager local to the OnStartup event

--- ModularWindowsAppExample/src/Copernicus.WpfApp/App.xaml.cs
+++ ModularWindowsAppExample/src/Copernicus.WpfApp/App.xaml.cs
@@ -9,14 +9,13 @@ namespace Copernicus.WpfApp;
 /// </summary>
 public partial class App : Application
 {
-    private ModuleManager _moduleManager;
     protected override void OnStartup(StartupEventArgs e)
     {
         var mainWindow = new MainWindow();
         mainWindow.Loaded += MainWindow_Loaded;
-        var _moduleManager = new ModuleManager(mainWindow);
-        mainWindow.Tag = _moduleManager;
-        _moduleManager.Load(mainWindow);
+        var moduleManager = new ModuleManager(mainWindow);
+        mainWindow.Tag = moduleManager;
+        moduleManager.Load(mainWindow);
         base.OnStartup(e);
         mainWindow.Show();
     }
