b895b6745d1e62b4a29e4619ee23a24611585540
Pete Voulgaris <petevoulgaris@gmail.com>
2025-04-02 21:26:25 -0400

    updated sampled with work on 'WhenAny' and 'WhenAnyValue' overloads

--- AvaloniaAppExample/src/AvaloniaAppExample/ViewModels/SandboxViewModel.cs
+++ AvaloniaAppExample/src/AvaloniaAppExample/ViewModels/SandboxViewModel.cs
@@ -2,6 +2,19 @@ using System;
 using System.Reactive;
 using ReactiveUI;
 
+/*
+ * Good question and answer:
+ * https://stackoverflow.com/questions/78687228/why-use-whenanyvalue-instead-of-raisepropertychanged
+ * 
+ * https://github.com/AvaloniaUI/Avalonia.Samples/tree/main/src/Avalonia.Samples/MVVM/BasicMvvmSample#step-2-add-properties-to-our-viewmodel-1
+ * 
+ * https://habr.com/en/articles/454074/
+ * 
+ * How to handle (possibly) multiple properties in:
+ * WhenAnyValue or WhenAny???
+ * 
+ */
+
 namespace AvaloniaAppExample.ViewModels
 {
     public class SandboxViewModel : ViewModelBase
@@ -16,11 +29,20 @@ namespace AvaloniaAppExample.ViewModels
                 x => !string.IsNullOrWhiteSpace(x) && x.Length > 7
                 );
 
+            IObservable<bool> foo = this.WhenAny(
+                x => x.DefaultText
+
+
+                , x => true);
+
             SubmitCommand = ReactiveCommand.Create(() =>
             {
                 DefaultText = "Clicked";
             }, isInputValid);
         }
+
+        public DateTime CurrentDate => DateTime.Now;
+
         public string Display { get; } = "Sandbox Area";
 
         public string DefaultText
