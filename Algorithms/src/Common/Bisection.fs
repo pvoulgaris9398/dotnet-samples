f65e529781f663a29123cc2026aa37de4f47d079
Pete Voulgaris <petevoulgaris@gmail.com>
2025-04-12 09:17:48 -0400

    added naive implementation of bisection method

---
+++ Algorithms/src/Common/Bisection.fs
@@ -0,0 +1,15 @@
+﻿module Bisection
+
+let epsilon = 1e-6
+
+// Handle case where a and b do not bracket the root
+let rec Solve (f: float -> float) (a: float) (b: float) =
+    let fa = f a
+    let fb = f b
+
+    if abs (b - a) < epsilon then
+        (a + b) / 2.0
+    else
+        let c = (a + b) / 2.0
+        let fc = f c
+        if (fa * fc < 0.0) then Solve f a c else Solve f c b
--- Algorithms/src/ConsoleTester/Program.fs
+++ Algorithms/src/ConsoleTester/Program.fs
@@ -1,2 +1,20 @@
-﻿open Plotly.NET
+﻿let f x = x * x - 2.0
+
+let a = 10.0
+let b = 20.0
+
+let root = Bisection.Solve f a b
+
+printfn $"Root %f{root}"
+
+let squares =
+    seq {
+        for i in 1..100 do
+            yield i * i
+    }
+
+for sq in squares do
+    printfn $"%d{sq}"
+
+open Plotly.NET
 Covariance.sampleChart |> Chart.show
