a4a683fe3d3af8c33957053782f7adf008af4fff
Pete Voulgaris <petevoulgaris@gmail.com>
2025-05-18 18:37:45 -0400

    Day 14 F# WIP

--- Algorithms/src/ConsoleTester/Day14.fs
+++ Algorithms/src/ConsoleTester/Day14.fs
@@ -10,6 +10,14 @@ let fileName = "..\\..\\..\\..\\..\\day14.txt"
 [<Literal>]
 let testData1 = "p=0,4 v=3,-3"
 
+type Location = { x: int64; y: int64 }
+
+type Velocity = { dx: int64; dy: int64 }
+
+type Robot =
+    { currentLocation: Location
+      velocity: Velocity }
+
 type DataPoint =
     | Point of x: int64 * y: int64
     | Velocity of dx: int64 * dy: int64
@@ -21,12 +29,24 @@ combine these types into one type
 let parseInstruction str =
     match str with
     | ParseRegex @"(?<point>p=)(?<x>\d+),(?<y>\d+)" [ _; ToInt32 x; ToInt32 y ] -> Point(x, y)
-    | ParseRegex @"(?<velocity>v=(?<dx>\d+),(?<dy>\d+))" [ _; ToInt32 dx; ToInt32 dy ] -> Velocity(dx, dy)
+    | ParseRegex @"(?<velocity>v=(?<dx>^-?\d+$),(?<dy>^-?\d+$))" [ _; ToInt32 dx; ToInt32 dy ] -> Velocity(dx, dy)
     | _ -> failwith "Unable to parse!"
 
-let getAllTokens (fileName: string) : List<DataPoint> =
+let parseInstruction2 str =
+    match str with
+    //@"(?<point>p=)(?<x>\d+),(?<y>\d+) (?<velocity>v=(?<dx>^-?\d+),(?<dy>^-?\d+))"
+    | ParseRegex @"(?p=(?<x>\d+),(?<y>\d+) v=(?<dx>^-?\d+),(?<dy>^-?\d+)" [ ToInt32 x; ToInt32 y; ToInt32 dx; ToInt32 dy ] ->
+        //Robot(currentLocation (x, y), velocity (dx, dy))
+
+
+        { currentLocation = { x = x; y = y }
+          velocity = { dx = dx; dy = dy } }
+
+    | _ -> failwith "Unable to parse!"
+
+let getAllTokens (fileName: string) : List<Robot> =
     let rx =
-        Regex(@"(?<point>p=)(?<x>\d+),(?<y>\d+)|(?<velocity>v=(?<dx>\d+),(?<dy>\d+))")
+        Regex(@"(?<point>p=)(?<x>\d+),(?<y>\d+) (?<velocity>v=(?<dx>^-?\d+),(?<dy>^-?\d+))")
 
     let m = rx.Matches <| testData1 //allText fileName
     (*
@@ -36,7 +56,7 @@ let getAllTokens (fileName: string) : List<DataPoint> =
     m
     |> Seq.cast<Match>
     |> Seq.map _.Value
-    |> Seq.map parseInstruction
+    |> Seq.map parseInstruction2
     |> Seq.toList
 
 ///
