3f5208bc3faf93edbe498e53fcb992cb2c83d7b0
Pete Voulgaris <petevoulgaris@gmail.com>
2025-05-19 18:43:40 -0400

    Day 14 F# ongoing WIP

--- Algorithms/src/ConsoleTester/Day14.fs
+++ Algorithms/src/ConsoleTester/Day14.fs
@@ -8,10 +8,10 @@ open System.Text.RegularExpressions
 let fileName = "..\\..\\..\\..\\..\\day14.txt"
 
 [<Literal>]
-let testData1 = "p=0,4 v=3,-3"
+let testData1 = "p=2,4 v=2,-3\r\n"
 
 [<Literal>]
-let testData2 = $"p=0,4 v=-23,7\np=12,33 v=11,-19"
+let testData2 = $"p=0,4 v=-23,7p=12,33 v=11,-19\r\n"
 
 type Location = { x: int64; y: int64 }
 
@@ -29,13 +29,29 @@ let parseData pattern str =
 
     | _ -> failwith "Unable to parse!"
 
-//let newLocation xmax ymax robot : bool =
-//    let newx = robot.currentLocation.x + robot.velocity.dx
-//    let newy = robot.currentLocation.y + robot.velocity.dy
+let newLocation xmax ymax robot : Robot =
+    let deltax = robot.currentLocation.x + robot.velocity.dx
+    let deltay = robot.currentLocation.y + robot.velocity.dy
 
-//    match robot with
-//    | { currentLocation.x = 101 } -> false
-//    | _ -> true
+    let newx =
+        match deltax with
+        | x when deltax < 0 -> xmax + deltax
+        | x when deltax >= 0 -> deltax
+        | _ -> deltax
+
+    let newy =
+        match deltay with
+        | x when deltay < 0 -> ymax + deltay
+        | x when deltay >= 0 -> deltay
+        | _ -> deltay
+
+    { currentLocation = { x = newx; y = newy }
+      velocity = robot.velocity }
+
+let moveOneFor (newLocation: Robot -> Robot) (iterations: int) (robot: Robot) : Robot =
+    [ 1..iterations ] |> Seq.fold (fun x _ -> newLocation x) robot
+
+let moveAllFor (moveOneFor: Robot -> Robot) (robots: seq<Robot>) = robots |> Seq.map moveOneFor
 
 let getAllTokens (data: string) : List<Robot> =
     let pattern = @"p=(?<x>\d+),(?<y>\d+) v=(?<dx>-?\d+),(?<dy>-?\d+)\r\n"
@@ -65,4 +81,69 @@ let Run (testing: bool) =
     if testing then
         allTokens |> List.iter (fun x -> printfn $"%A{x}")
 
+    let newLocationConfigured = newLocation 101 103
+    let moveOneForConfigured = moveOneFor newLocationConfigured 100
+
+    let locations =
+        allTokens |> List.toSeq |> moveAllFor (fun x -> moveOneForConfigured x)
+
+    locations |> Seq.iter (fun x -> printfn $"%A{x}")
+
+    (*
+    TODO: Calculate how many are in each quadrant,
+    multiply these four (4) sums by each other to calculate
+    (Any robots along the center lines (horizontal/vertical) do not count
+    the "Safety Factor"
+    *)
+
     printfn "\nDay14 - DONE\n"
+
+let test2 =
+    let data = testData1
+
+    let newLocationConfigured = newLocation 11 7
+
+    let allTokens = getAllTokens data
+
+    let robot = allTokens.Head
+
+    let final = moveOneFor newLocationConfigured 4 robot
+
+    printfn $"final: %A{final}"
+
+    ()
+
+let test1 testing =
+
+    let data = testData1
+
+    let newLocationConfigured = newLocation 11 7
+
+    let allTokens = getAllTokens data
+
+    let robot = allTokens.Head
+
+    if testing then
+        printfn $"%A{robot}"
+
+    let newLocation1 = newLocationConfigured robot
+
+    if testing then
+        printfn $"newLocation1: %A{newLocation1}"
+
+    let newLocation2 = newLocationConfigured newLocation1
+
+    if testing then
+        printfn $"newLocation2: %A{newLocation2}"
+
+    let newLocation3 = newLocationConfigured newLocation2
+
+    if testing then
+        printfn $"newLocation3: %A{newLocation3}"
+
+    let newLocation4 = newLocationConfigured newLocation3
+
+    if testing then
+        printfn $"newLocation4: %A{newLocation4}"
+
+    ()
