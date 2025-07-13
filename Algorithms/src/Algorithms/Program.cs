2f0b3b8e98dc6e575b8994021b789ccc894fbd9b
Pete Voulgaris <petevoulgaris@gmail.com>
2025-07-13 17:32:57 -0400

    added sample on priority queue and Dijkstra's algorithm

--- Algorithms/src/Algorithms/Program.cs
+++ Algorithms/src/Algorithms/Program.cs
@@ -1,10 +1,9 @@
 
+using Algorithms;
+
 var quotient = 8;
 
-//foreach (var i in Enumerable.Range(0, 100))
-//{
-//    Console.WriteLine($"{nameof(i)} modulo {quotient} = {i % quotient}");
-//}
+PriorityQueueExample.Run();
 
 foreach (var i in Enumerable.Range(0, 100))
 {
