c4fd554d86729bc7fcad321bc534830cd73ab3a5
Pete Voulgaris <petevoulgaris@gmail.com>
2025-08-17 15:25:18 -0400

    ongoing WIP

--- AdventOfCode/2024/src/CSharp/Day20.cs
+++ AdventOfCode/2024/src/CSharp/Day20.cs
@@ -19,73 +19,50 @@
             var stepCount = maze.Walk().Count();
 
             WriteLine($"{nameof(stepCount)}: {stepCount}");
+
+            var cheats = maze.Cheats().ToList();
+
+
         }
 
         private static char[][] ReadMaze(this TextReader text) =>
             [.. text.ReadLines().Select(line => line.ToCharArray())];
 
-        private static Point GetStartPosition(this char[][] maze) => maze.FindAll('S').First();
+        private static Point GetStartPosition(this char[][] maze) => maze.AllPoints().First(p => p.Value == 'S');
 
-        private static Point GetEndPosition(this char[][] maze) => maze.FindAll('E').First();
+        private static Point GetEndPosition(this char[][] maze) => maze.AllPoints().First(p => p.Value == 'E');
 
         private static bool Contains(this char[][] maze, (int Row, int Column) point) =>
             point.Row >= 0 && point.Row < maze.Length &&
             point.Column >= 0 && point.Column < maze[point.Row].Length;
 
-        private static IEnumerable<Point> FindAll(this char[][] maze, char content) =>
+        private static IEnumerable<Point> AllPoints(this char[][] maze) =>
             from row in Enumerable.Range(0, maze.Length)
             from column in Enumerable.Range(0, maze[0].Length)
-            where maze[row][column] == content
-            select new Point(row, column);
+            select new Point(row, column, maze[row][column]);
 
         private static HashSet<(int Row, int Column)> Visited = [];
 
         /*
          * Any sequence of three Points where 1 -> 2 -> 3 where 1 = '.' and 3 = '.' and 2 = '#'
          */
-        private static IEnumerable<(Point first, Point second)> PossibleCheats(this char[][] maze) => [];
 
-        private static IEnumerable<(Point first, Point second)> HorizontalCheats(this char[][] maze) =>
+        private static IEnumerable<(Point Start, Point Finish)> Cheats(this char[][] maze) =>
+            maze.HorizontalCheats().Concat(maze.VerticalCheats());
+
+        private static IEnumerable<(Point Start, Point Finish)> VerticalCheats(this char[][] maze) =>
+            Enumerable.Range(0, maze[0].Length)
+                .Select(columnIndex => maze.AllPoints().Where(p => p.Column == columnIndex).ToList())
+                .Take(3)
+                .Where(x => x[0].Value == '.' && x[1].Value == '#' && x[2].Value == '.')
+                .Select(x => (x[0], x[2]));
+
+        private static IEnumerable<(Point Start, Point Finish)> HorizontalCheats(this char[][] maze) =>
             Enumerable.Range(0, maze.Length)
-            .SelectMany(rowIndex => maze[rowIndex].HorizontalCheats(rowIndex));
-
-
-        private static IEnumerable<(Point first, Point second)> VerticalCheats(this char[][] maze) => [];
-
-        /*
-         * WIP - Most likely doesn't actually work
-         */
-        private static IEnumerable<(Point first, Point second)> HorizontalCheats(this char[] line, int rowIndex)
-        {
-            var enumerator = line.GetEnumerator();
-
-            if (!enumerator.MoveNext())
-            {
-                yield break;
-            }
-
-            int prev = (int)enumerator.Current;
-
-            if (!enumerator.MoveNext())
-            {
-                yield break;
-            }
-
-            int middle = (int)enumerator.Current;
-
-            while (enumerator.MoveNext())
-            {
-                int next = (int)enumerator.Current;
-
-                if (line[prev] == '.' && line[middle] == '#' && line[next] == '.')
-                {
-                    yield return (new Point(rowIndex, prev), new Point(rowIndex, next));
-                }
-                prev = middle;
-                middle = next;
-            }
-
-        }
+                .Select(rowIndex => maze.AllPoints().Where(p => p.Row == rowIndex).ToList())
+                .Take(3)
+                .Where(x => x[0].Value == '.' && x[1].Value == '#' && x[2].Value == '.')
+                .Select(x => (x[0], x[2]));
 
         private static Point Next(this Point point, char[][] maze) =>
             new[]
@@ -94,7 +71,7 @@
             (point.Row, point.Column - 1), (point.Row, point.Column + 1)
             }
             .Where(neighbor => (maze.At(neighbor) == '.' || maze.At(neighbor) == 'E') && !Visited.Contains(neighbor))
-            .Select(p => new Point(p.Item1, p.Item2))
+            .Select(p => new Point(p.Item1, p.Item2, maze[p.Item1][p.Item2]))
             .First();
 
         private static char At(this char[][] map, (int Row, int Column) point) =>
@@ -113,6 +90,6 @@
             }
         }
 
-        internal sealed record Point(int Row, int Column);
+        internal sealed record Point(int Row, int Column, char Value);
     }
 }
