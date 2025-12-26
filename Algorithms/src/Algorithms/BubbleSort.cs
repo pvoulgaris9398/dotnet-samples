39d557e818817fe6e65f21d0e6cff36f89748d29
Pete Voulgaris <petevoulgaris@gmail.com>
2025-12-26 13:26:58 -0500

    added bubble-short implementation

---
+++ Algorithms/src/Algorithms/BubbleSort.cs
@@ -0,0 +1,71 @@
+#!/usr/bin/env -S dotnet run
+
+/*
+WIP
+*/
+if (args.Length > 0)
+{
+    Action actionToExecute = args[0] switch
+    {
+        "test0" => Tests.Test0,
+        _ => () => Console.WriteLine("Unrecognized command-line argument"),
+    };
+    actionToExecute();
+    return;
+}
+
+internal static class BubbleSort
+{
+    internal static void Sort(int[] input)
+    {
+        if (input is { Length: > 0 })
+        {
+            int n = input.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                bool swapped = false;
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (input[j] > input[j + 1])
+                    {
+                        (input[j], input[j + 1]) = (input[j + 1], input[j]);
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
+
+internal static class Tests
+{
+    internal static void Test0()
+    {
+        int[] input = [1, 344, 222, 666, 11, 25, 12, 444, 6, 23, 65, 2];
+
+        Console.WriteLine("Before:");
+        input.Print();
+
+        BubbleSort.Sort(input);
+
+        Console.WriteLine("After:");
+        input.Print();
+    }
+}
+
+// TODO: Move to the "Common" assembly and reference from there
+internal static class ListExtensions
+{
+    public static void Print(this int[] input)
+    {
+        foreach (int element in input)
+        {
+            Console.Write($"{element},");
+        }
+        Console.WriteLine();
+    }
+}
--- BenchmarkResearch/src/BenchmarkResearch/Program.cs
+++ BenchmarkResearch/src/BenchmarkResearch/Program.cs
@@ -1,5 +1,4 @@
 ﻿using BenchmarkDotNet.Running;
-
 using BenchmarkResearch;
 
 BenchmarkRunner.Run<StringLoaderBenchmark>();
