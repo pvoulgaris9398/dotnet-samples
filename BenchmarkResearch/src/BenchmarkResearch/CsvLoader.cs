d36d4a06636976d590e40415410974ee4a8596ee
Pete Voulgaris <petevoulgaris@gmail.com>
2025-12-30 18:38:22 -0500

    added some additional supporting code for my benchmark research task

---
+++ BenchmarkResearch/src/BenchmarkResearch/CsvLoader.cs
@@ -0,0 +1,24 @@
+using System.Globalization;
+using CsvHelper;
+
+#pragma warning disable IDE0160 // Convert to block scoped namespace
+namespace BenchmarkResearch;
+#pragma warning restore IDE0160 // Convert to block scoped namespace
+
+public sealed class CsvLoader
+{
+    public void Process()
+    {
+        string fileName = @"C:\Users\Peter\_work\_tempdata\Crime_Data_from_2020_to_Present.csv";
+
+        using var reader = new StreamReader(fileName);
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+        _ = csv.Read();
+        _ = csv.ReadHeader();
+        while (csv.Read())
+        {
+            Console.WriteLine(csv.GetField<string>("AREA"));
+        }
+    }
+}
--- BenchmarkResearch/src/BenchmarkResearch/Program.cs
+++ BenchmarkResearch/src/BenchmarkResearch/Program.cs
@@ -1,4 +1,30 @@
 ﻿using BenchmarkDotNet.Running;
 using BenchmarkResearch;
 
-BenchmarkRunner.Run<StringLoaderBenchmark>();
+if (args.Length > 0)
+{
+    Action actionToExecute = args[0] switch
+    {
+        "test0" => Tests.Test0,
+        "test1" => Tests.Test1,
+        _ => () => Console.WriteLine("Unrecognized command-line argument"),
+    };
+    actionToExecute();
+    return;
+}
+
+internal static class Tests
+{
+    public static void Test0()
+    {
+        Console.WriteLine("Test0: CsvLoader Process");
+        new CsvLoader().Process();
+    }
+
+    public static void Test1()
+    {
+        BenchmarkRunner.Run<StringLoaderBenchmark>();
+        Console.WriteLine("Test1: ");
+    }
+
+}
