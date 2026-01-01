866b7979c7f455d7e3c66d297cb681fb0471c67b
Pete Voulgaris <petevoulgaris@gmail.com>
2026-01-01 13:56:58 -0500

    got benchmark sample working, need to validate and understand it

--- BenchmarkResearch/src/BenchmarkResearch/BenchmarkDataRow.cs
+++ BenchmarkResearch/src/BenchmarkResearch/BenchmarkDataRow.cs
@@ -2,4 +2,4 @@
 namespace BenchmarkResearch;
 
 #pragma warning restore IDE0160 // Convert to block scoped namespace
-public sealed record DataRow(DateOnly Date, decimal Close, string Name);
+public sealed record BenchmarkDataRow(string Date, string Close, string Name);
--- BenchmarkResearch/src/BenchmarkResearch/CsvBenchmark.cs
+++ BenchmarkResearch/src/BenchmarkResearch/CsvBenchmark.cs
@@ -1,19 +1,32 @@
-using System.Data;
 using System.Globalization;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
 using CsvHelper;
 using CsvHelper.Configuration;
+using nietras.SeparatedValues;
 
 #pragma warning disable IDE0160 // Convert to block scoped namespace
 namespace BenchmarkResearch;
 
 #pragma warning restore IDE0160 // Convert to block scoped namespace
 
-private static string FileName => @"C:\Users\Peter\_work\_tempdata\stocks\all_stocks_5yr.csv";
-
-public sealed class CsvBenchmark
+[MemoryDiagnoser]
+public class CsvBenchmark
 {
+    private static string FileName => @"C:\Users\Peter\_work\_tempdata\stocks\all_stocks_5yr.csv";
+
+    private readonly Consumer _consumer = new();
+
+    [Params(1, 100, 100, 10_000, 50_000, 100_000, 150_000, 500_000)]
+    public int Rows { get; set; }
+
     [Benchmark]
-    public void TestCsvHelper()
+    public void RunTestCsvHelper() => TestCsvHelper().Consume(_consumer);
+
+    [Benchmark]
+    public void RunTestSep() => TestSep().Consume(_consumer);
+
+    internal IEnumerable<BenchmarkDataRow> TestCsvHelper()
     {
         CsvConfiguration config = new(CultureInfo.InvariantCulture)
         {
@@ -26,7 +39,7 @@ public sealed class CsvBenchmark
         EnumerableTextReader with file contents as IEnumerable<string> or below:
         */
 
-        using var reader = new StreamReader(fileName);
+        using var reader = File.OpenText(FileName);
         using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
 
         _ = csv.Read();
@@ -42,18 +55,25 @@ public sealed class CsvBenchmark
             Want to see if we can reduce memory usage at all here:
             https://github.com/JoshClose/CsvHelper
             */
-            yield return new(csv.GetField("date"), csv.GetField("close"), csv.GetField("Name"));
+            yield return new(
+                csv.GetField("date") ?? "",
+                csv.GetField("close") ?? "",
+                csv.GetField("Name") ?? ""
+            );
         }
     }
 
-    [Benchmark]
-    public IEnumerable<DataRow> TestSep()
+    internal IEnumerable<BenchmarkDataRow> TestSep()
     {
-        using var reader = Sep.Reader().FromFile(fileName);
+        using var reader = Sep.Reader().FromFile(FileName);
 
         foreach (var row in reader)
         {
-            yield return new(row["date"].Span, row["close"].Span, row["Name"].Span);
+            yield return new(
+                row["date"].ToString(),
+                row["close"].ToString(),
+                row["Name"].ToString()
+            );
         }
     }
 }
